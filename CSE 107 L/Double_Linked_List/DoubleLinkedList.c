c99df6d4fa7dd61ae20fdc27626e1c863e61a2b3
Jenil Padshala <jenilpadshala@gmail.com>
2022-04-11 16:19:52 +0530

    11th April 2022

--- CSE 107 L/Double_Linked_List/DoubleLinkedList.c
+++ CSE 107 L/Double_Linked_List/DoubleLinkedList.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dbl_list
+struct dbl_list //Defining Doubly linked list node 
 {
-    int data;
-    struct dbl_list *prev,
-                    *next;
+    int data;   //Stores the integer data
+    struct dbl_list *prev,  //pointer to store location of previous linked list node
+                    *next;  //pointer to store location of next linked list node
 };
 
 int main()
 {
-    struct dbl_list *head = NULL,
-                    *p;
-    for(int i=0; i<3; i++)
+    struct dbl_list *head = NULL,   //Pointer for starting location of linked list
+                    *p;             //Pointer for new node created
+    for(int i=0; i<3; i++)      /*Input data elements from users and store in linked list*/
     {
-        p = (struct dbl_list*)malloc(sizeof(struct dbl_list));
+        p = (struct dbl_list*)malloc(sizeof(struct dbl_list));  //Creating new node
         printf("Enter element %d:", i+1);
-        scanf("%d", &p->data);
+        scanf("%d", &p->data);      //Printing data
         p->prev = NULL;
         p->next = NULL;
 
@@ -44,13 +44,14 @@ int main()
         head = head->next;
     }*/
     
-    if (head->prev != NULL)
+    /*traversing back to 1st element*/
+    
+    while (head->prev != NULL)
     {
-        while (head->prev != NULL)
-        {
-            head = head -> prev;
-        }
+        head = head -> prev;
     }
+
+    /*Printing data stored in doubly linked list*/
     do
     {
         printf("Element is %d\n", head->data);
