b22c5149bbbbda2ef3fb21d99e79c48b9eb8e540
Jenil Padshala <jenilpadshala@gmail.com>
2022-09-06 11:58:11 +0530

    6th September 2022

--- Sem 3/CSE 201/Knight/knightTour.cpp
+++ Sem 3/CSE 201/Knight/knightTour.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     int Board [8][8];
     int Moves [8][2] = {{-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}};
+    
     //Assigned 0 as elements in Board
     for (int i=0; i<8; i++)
     {
@@ -16,10 +19,13 @@ int main()
     }
 
     //Generating random position of Knight
+    srand(time(0));
+    int i =rand()%8;
+    int j =rand()%8;
 
-    int i = 5;//rand()%8 + 1;
-    int j = 5;//rand()%8 + 1;
+    //Placing Horse at position 1
     Board[i][j] = 1;
+    
     int k=0, count = 2;
     int Newi, Newj;
 
@@ -30,20 +36,10 @@ int main()
 
         if (Newi >= 0 && Newi <=7 && Newj >= 0 && Newj <= 7 && Board[Newi][Newj] == 0)
         {
-            Board[Newi][Newj] = count++;
+            Board[Newi][Newj] = count++; 
             i = Newi;
             j = Newj;
             k = 0;
-            //Display the Board
-    for (int x=0; x<8; x++)
-    {
-        for (int y=0; y<8; y++)
-        {
-            cout << Board[x][y]<< "  ";
-        }
-        cout << endl;
-    }
-    cout << endl<< endl;
         }
         else
         {
@@ -52,14 +48,14 @@ int main()
 
     }while(k<8);
 
-    /*//Display the Board
+    
     for (int x=0; x<8; x++)
     {
         for (int y=0; y<8; y++)
         {
-            cout << Board[x][y]<< "  ";
+            cout <<setw(2)<< Board[x][y]<< "  ";
         }
         cout << endl;
-    }*/
+    }
 
 }
